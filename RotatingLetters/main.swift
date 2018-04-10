9d0e4c6262d77ecd0352de4ec8a8bab329ea87fb
Maxwell Anna <amaxwell@lcs-mba3091.lcs.on.ca>
2018-04-10 09:20:43 -0400

    rotating letters final

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -11,7 +11,68 @@ import Foundation
 // Get the user input
 var rawInput = readLine()
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+
+
+// Make sure input was given (create a string from the string)
+guard let input = rawInput else {
+    //Error
+    exit(9)
+}
+
+
+
+// process
+var rotatingLetter = 0
+var nonRotatingLetter = 0
+for letters in input {
+    print(letters)
+    
+    
+    
+    switch letters {
+    case "I", "O", "S", "H", "Z", "X", "N" :
+        rotatingLetter += 1
+        
+    case "A", "B", "C", "D", "E", "F", "G", "J", "K", "L", "M", "P", "Q", "R", "T",
+         "U", "V", "W", "Y", " " :
+        nonRotatingLetter += 1000
+        
+    case "a", "b", "c", "d", "e", "f", "g", "j", "k", "l", "m", "p", "q", "r", "t",
+         "u", "v", "w", "y", " " :
+        
+        nonRotatingLetter += 1000
+        
+    default:
+        break
+    }
+    
+}
+
+// output
+if rotatingLetter > nonRotatingLetter {
+    print("Yes")
+} else if nonRotatingLetter > rotatingLetter {
+    print("No")
+} else {
+    print("Unsure")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
